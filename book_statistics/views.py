7acaf6ce61c8dcf75259da7c7295b1e03bc96474
Aaron Verber <aaron.verber@gmail.com>
2012-05-09 21:48:34 -0500

    renamed files, added loop

--- book_statistics/views.py
+++ book_statistics/views.py
@@ -14,9 +14,10 @@ from django.template import RequestContext
 import nltk
 from nltk.util import islice
 import pylab 
+import os
 
 
-def _create_plot(text, words):
+def _create_plot(file, text, words):
 	text = list(text) 
 	words.reverse() 
 	points = [(x,y) for x in range(len(text)) 
@@ -26,34 +27,15 @@ def _create_plot(text, words):
 		x,y = zip(*points)
 	else:
 		x = y = ()
-	pylab.figure(0)
+	pylab.figure()
 	pylab.plot(x, y, "b|", scalex=.1) 
 	pylab.yticks(range(len(words)), words, color="b") 
 	pylab.ylim(-1, len(words)) 
 	pylab.title("Lexical Dispersion Plot") 
 	pylab.xlabel("Word Offset") 
-	pylab.savefig('public/dispersion.png')
+	pylab.savefig('public/graphs/%s_dispersion.png' % file)
 
-def _frequency_plot(text): 
-	""" 
-	Plot samples from the frequency distribution 
-	displaying the most frequent sample first.  If an integer 
-	parameter is supplied, stop after this many samples have been 
-	plotted.  If two integer parameters m, n are supplied, plot a 
-	subset of the samples, beginning with m and stopping at n-1. 
-	For a cumulative plot, specify cumulative=True. 
-	(Requires Matplotlib to be installed.) 
-	@param title: The title for the graph 
-	@type title: C{str} 
-	@param cumulative: A flag to specify whether the plot is cumulative (default = False) 
-	@type title: C{bool} 
-	""" 
-	try: 
-		import pylab 
-	except ImportError: 
-		raise ValueError('The plot function requires the matplotlib package (aka pylab).' 
-					 'See http://matplotlib.sourceforge.net/') 
-	 
+def _frequency_plot(file, text): 
 	samples = list(islice(text, 25)) 
 	 
 	cumulative = True 
@@ -69,21 +51,25 @@ def _frequency_plot(text):
 	if "title" in kwargs: 
 		pylab.title(kwargs["title"]) 
 		del kwargs["title"] 
-	pylab.figure(1)
+	pylab.figure()
 	pylab.plot(freqs, **kwargs) 
 	pylab.xticks(range(len(samples)), [str(s) for s in samples], rotation=90) 
 	pylab.xlabel("Samples") 
 	pylab.ylabel(ylabel) 
-	pylab.savefig('public/frequency.png')
+	pylab.savefig('public/graphs/%s_frequency.png' % file)
 
 def dispersion(request):
-	raw = open("documents/591_16cac_textbook.txt").read()
-	tokens = nltk.word_tokenize(raw)
-	text = nltk.Text(tokens)
-	_create_plot(text,["users", "groups", "ntfs", "permissions"])
-	raw = open("documents/591_16cac_textbook.txt").read()
-	tokens = nltk.word_tokenize(raw)
-	text = nltk.Text(tokens)
-	fdist1 = nltk.FreqDist(text)
-	_frequency_plot(fdist1)
-	return render_to_response("index.html", {}, context_instance = RequestContext(request))
+	files = os.listdir('documents')
+	for file in files:
+		raw = open("documents/%s" % file).read()
+		tokens = nltk.word_tokenize(raw)
+		text = nltk.Text(tokens)
+		_create_plot(file, text,["users", "groups", "ntfs", "permissions"])
+		
+		raw = open("documents/%s" % file).read()
+		tokens = nltk.word_tokenize(raw)
+		text = nltk.Text(tokens)
+		fdist1 = nltk.FreqDist(text)
+		_frequency_plot(file, fdist1)
+		
+	return render_to_response("index.html", {'chapters' : [1,2,3,4]}, context_instance = RequestContext(request))
