317d8d925dc5948c9da3f2d531e65a687d6698ff
Aaron Verber <aaron.verber@gmail.com>
2012-05-09 21:13:17 -0500

    nav intact

--- book_statistics/views.py
+++ book_statistics/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
 # def home(args):
@@ -12,6 +12,7 @@ from django.template import RequestContext
 # def hello_world(args):
 # 	return render_to_response('index.html')
 import nltk
+from nltk.util import islice
 import pylab 
 
 
@@ -25,6 +26,7 @@ def _create_plot(text, words):
 		x,y = zip(*points)
 	else:
 		x = y = ()
+	pylab.figure(0)
 	pylab.plot(x, y, "b|", scalex=.1) 
 	pylab.yticks(range(len(words)), words, color="b") 
 	pylab.ylim(-1, len(words)) 
@@ -32,9 +34,62 @@ def _create_plot(text, words):
 	pylab.xlabel("Word Offset") 
 	pylab.savefig('public/dispersion.png')
 
+def _frequency_plot(text, dist): 
+	""" 
+	Plot samples from the frequency distribution 
+	displaying the most frequent sample first.  If an integer 
+	parameter is supplied, stop after this many samples have been 
+	plotted.  If two integer parameters m, n are supplied, plot a 
+	subset of the samples, beginning with m and stopping at n-1. 
+	For a cumulative plot, specify cumulative=True. 
+	(Requires Matplotlib to be installed.) 
+	@param title: The title for the graph 
+	@type title: C{str} 
+	@param cumulative: A flag to specify whether the plot is cumulative (default = False) 
+	@type title: C{bool} 
+	""" 
+	try: 
+		import pylab 
+	except ImportError: 
+		raise ValueError('The plot function requires the matplotlib package (aka pylab).' 
+					 'See http://matplotlib.sourceforge.net/') 
+	 
+	samples = list(islice(text, dist)) 
+	 
+	cumulative = True
+	if cumulative: 
+		freqs = list(text._cumulative_frequencies(samples)) 
+		ylabel = "Cumulative Counts" 
+	else: 
+		freqs = [text[sample] for sample in samples] 
+		ylabel = "Counts" 
+	# percents = [f * 100 for f in freqs]  only in ProbDist? 
+	 
+	pylab.grid(True, color="silver") 
+
+	kwargs = {}
+	kwargs["linewidth"] = 2 
+	if "title" in kwargs: 
+		pylab.title(kwargs["title"]) 
+		del kwargs["title"] 
+	pylab.figure(1)
+	pylab.plot(freqs, **kwargs) 
+	pylab.xticks(range(len(samples)), [str(s) for s in samples], rotation=90) 
+	pylab.xlabel("Samples") 
+	pylab.ylabel(ylabel) 
+	pylab.savefig('public/frequency.png')
+
 def dispersion(request):
 	raw = open("documents/591_16cac_textbook.txt").read()
 	tokens = nltk.word_tokenize(raw)
 	text = nltk.Text(tokens)
 	_create_plot(text,["users", "groups", "ntfs", "DNS"])
 	return render_to_response("index.html", {}, context_instance = RequestContext(request))
+
+def frequency(request):
+	raw = open("documents/591_16cac_textbook.txt").read()
+	tokens = nltk.word_tokenize(raw)
+	text = nltk.Text(tokens)
+	fdist1 = nltk.FreqDist(text)
+	_frequency_plot(fdist1, 50)
+	return redirect('/')
